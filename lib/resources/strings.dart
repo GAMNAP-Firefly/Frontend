dc98c2776c0a54e0c38648af98347d6a2797286f
agapo <agapova.nelli@gmail.com>
2025-07-07 16:07:26 +0300

    feat(test-result-mobile): create presentation layer p1-44

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -5,5 +5,9 @@ class Strings {
   static const String ansNotSure = "Не знаю";
   static const String ansWrong = "Неверно";
   static const String backButton = "Назад";
+  static const String backToMainScreenButton = "На главный экран";
   static const String questionMock = "Вы когда-нибудь мечтали стать лучшей версией себя?";
+  static const String resMockImage = "assets/images/mock_image.png";
+  static const String resTextMock = "🔥Вы - Агапова Нелли Витальевна!🔥";
+  static const String resDescMock = "✅ Это очень редкий типаж крутых дефф4онок разработчиков💅, гордитесь собой😎";
 }
